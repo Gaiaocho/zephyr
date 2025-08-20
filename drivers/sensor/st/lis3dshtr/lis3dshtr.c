a328ebcb2f15e84aff134ada3555d8490eb9b6cf
Zacck Osiemo <coderv63@gmail.com>
2025-08-20 10:20:59 +0200

    drivers: sensors: Fix ST_LIS3DSHTR
    Syntax and building fixes for the accelerometer driver
    
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- drivers/sensor/st/lis3dshtr/lis3dshtr.c
+++ drivers/sensor/st/lis3dshtr/lis3dshtr.c
@@ -22,7 +22,7 @@ struct st_lis3dshtr_data {
 	uint8_t sample_dec;
 };
 
-static int lis3dshtr_write_register(const struct device *dev, uint8_t reg, uint16_t value) {
+static int lis3dshtr_write_register(const struct device *dev, uint8_t reg, uint8_t value) {
 	const struct st_lis3dshtr_config *cfg = dev->config;
 
 	uint8_t tx_buf[2] = {reg,value};
@@ -37,7 +37,7 @@ static int lis3dshtr_write_register(const struct device *dev, uint8_t reg, uint1
 	};
 
 
-	return spi_write_dt(&cfg->spi_controller), &tx);
+	return spi_write_dt(&cfg->spi_controller, &tx);
 }
 
 static int lis3dshtr_read_register(const struct device *dev, uint8_t reg, uint8_t *val)  {
@@ -66,11 +66,11 @@ static int lis3dshtr_read_register(const struct device *dev, uint8_t reg, uint8_
 		.count = 1,
 	};
 
-	ret = spi_tranceive_dt(&cfg->spi_controller, &tx_set, &rx_set);
+	ret = spi_transceive_dt(&cfg->spi_controller, &tx_set, &rx_set);
 
 	if (ret != 0) {
 		LOG_ERR("Error reading channel axis (%02X)", reg);
-		return ret;,
+		return ret;
 	}
 
 	return  ret;
@@ -79,29 +79,30 @@ static int lis3dshtr_read_register(const struct device *dev, uint8_t reg, uint8_
 static int lis3dshtr_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
 	struct st_lis3dshtr_data *data = dev->data;
-	const struct st_lis3dshtr_config *config = dev->config;
 	int ret;
 
-	if(chan ! = SENSOR_CHAN_ACCEL_X) {
+	if (chan != SENSOR_CHAN_ACCEL_X) {
 		return -ENOTSUP;
 	}
 
-	ret = lis3dshtr_read_register(&config->spi_controller, 0x29, data->sample_int);
+	ret = lis3dshtr_read_register(dev, 0x29, &data->sample_int);
 
 	if (ret != 0) {
 		LOG_ERR("Could not read channel HIGH byte");
 		return ret;
 	}
 
-	ret = lis3dshtr_read_register(&config->spi_controller, 0x28, data->sample_int);
+	ret = lis3dshtr_read_register(dev, 0x28, &data->sample_int);
 
 	if (ret != 0) {
 		LOG_ERR("Could not read channe LOW byte");
 		return ret;
 	}
+
+	return ret;
 }
 
-static int lis3dshtr_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_val *val)
+static int lis3dshtr_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val)
 {
 	struct st_lis3dshtr_data *data = dev->data;
 	int16_t raw;
@@ -111,7 +112,7 @@ static int lis3dshtr_channel_get(const struct device *dev, enum sensor_channel c
 		return -ENOTSUP;
 	}
 
-	/*Combine high and low byte*
+	/*Combine high and low byte*/
 	raw = (int16_t)((data->sample_int << 8) | data->sample_dec);
 
 	/*Apply 2G scaling*/
@@ -128,7 +129,7 @@ static int lis3dshtr_channel_get(const struct device *dev, enum sensor_channel c
 static DEVICE_API(sensor, st_lis3dshtr_api) = {
 	.sample_fetch = lis3dshtr_sample_fetch,
 	.channel_get = lis3dshtr_channel_get,
-}
+};
 
 static int st_lis3dshtr_init(const struct device *dev)
 {
@@ -138,7 +139,7 @@ static int st_lis3dshtr_init(const struct device *dev)
 
 	if(!spi_is_ready_dt(&config->spi_controller)) {
 		LOG_ERR("SPI Bus not ready");
-		return -ENODEVl;
+		return -ENODEV;
 	}
 
 	/*Static bring up*/
@@ -155,10 +156,10 @@ static int st_lis3dshtr_init(const struct device *dev)
 }
 
 #define SENSOR_LIS3DSHTR_DEFINE(inst)							\
-	static struct st_lis3dshtr_data lis3dshtr_data_##inst				\
+	static struct st_lis3dshtr_data lis3dshtr_data_##inst;				\
 	static struct st_lis3dshtr_config lis3dshtr_config_##inst = {			\
 		.spi_controller  = SPI_DT_SPEC_INST_GET(inst, SPI_WORD_SET(8), 0),	\
-	}										\
+	};										\
 											\
 	SENSOR_DEVICE_DT_INST_DEFINE(inst,						\
 				     &st_lis3dshtr_init,				\
