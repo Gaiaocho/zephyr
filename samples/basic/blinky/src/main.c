53f402417ac65809ddc25c4d8e893dda926c2c9b
Zacck Osiemo <coderv63@gmail.com>
2026-01-18 05:46:32 +0200

    kitchen sink commit for rue's build
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -6,43 +6,46 @@
 
 #include <stdio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
 	int ret;
-	bool led_state = true;
+	printf("Does this work Rue?");
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
+	// talk to expander
+	const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
+
+	if(i2c_dev == NULL || !device_is_ready(i2c_dev)) {
+		printf("I2C peripheral not ready\n");
+		return -1;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
+	while(1) {
+		ret = i2c_reg_write_byte(i2c_dev, 0x20, 0x7, (0x7 >> 8));
+		if(ret != 0) {
+			printf("Could not enable all 3 LEDs \n");
+			return -1;
 		}
-
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
+
+		ret = i2c_reg_write_byte(i2c_dev, 0x20, 0x0, (0x0 >> 8));
+		if(ret != 0) {
+			printf("Could not enable all 3 LEDs \n");
+			return -1;
+		}
 	}
+
 	return 0;
 }
